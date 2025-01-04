8e33bcb7f27d6fc1833774dfd4fc68ab0050fdf4
Tayo <teeydigba@gmail.com>
2025-01-04 09:01:59 +0100

    added footer to publication page

--- src/pages/Publication/Desci.jsx
+++ src/pages/Publication/Desci.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./publication.css";
+import Footer from "../../components/Footer";
 
 const Desci = () => {
   return (
@@ -7,20 +8,69 @@ const Desci = () => {
       <div className="publication">
         <h1>A Case for Decentralized Science.</h1>
         <p>
-          The emergence of memecoins has sparked significant debate, particularly regarding their implications for the scientific community. Critics often dismiss memecoins as frivolous, arguing that they undermine serious scientific endeavours. However, a closer examination reveals a more nuanced perspective, one that acknowledges the cultural and economic dynamics at play.<br/><br/>
-          It is interesting to know that the origin of “meme” is scientific. Originally coined by evolutionary biologist, Richard Dawkins, in his 1976 book “The Selfish Gene”. Dawkins described memes as units of cultural transmission, akin to genes in biological evolution, capable of replicating and evolving through social interactions. <br/><br/>
+          The emergence of memecoins has sparked significant debate,
+          particularly regarding their implications for the scientific
+          community. Critics often dismiss memecoins as frivolous, arguing that
+          they undermine serious scientific endeavours. However, a closer
+          examination reveals a more nuanced perspective, one that acknowledges
+          the cultural and economic dynamics at play.
+          <br />
+          <br />
+          It is interesting to know that the origin of “meme” is scientific.
+          Originally coined by evolutionary biologist, Richard Dawkins, in his
+          1976 book “The Selfish Gene”. Dawkins described memes as units of
+          cultural transmission, akin to genes in biological evolution, capable
+          of replicating and evolving through social interactions. 
+          <br />
+          <br />
         </p>
         <p>
-          His foundational concept highlights that memes — memecoins being a derivative — are not merely trivial; they represent a significant aspect of human culture and communication. A worthy mention is Dogecoin. Launched in 2013 but has transcended being just an internet sensation to influencing broader cultural narratives 11 years later. This is particularly evident in the creation of the Department of Government Efficiency (D.O.G.E), humorously named after the cryptocurrency, which Elon Musk, an open advocate for Dogecoin is set to lead.<br/>
+          His foundational concept highlights that memes — memecoins being a
+          derivative — are not merely trivial; they represent a significant
+          aspect of human culture and communication. A worthy mention is
+          Dogecoin. Launched in 2013 but has transcended being just an internet
+          sensation to influencing broader cultural narratives 11 years later.
+          This is particularly evident in the creation of the Department of
+          Government Efficiency (D.O.G.E), humorously named after the
+          cryptocurrency, which Elon Musk, an open advocate for Dogecoin is set
+          to lead.
+          <br />
         </p>
         <p>
-          Also, prominent figures in the cryptocurrency space have weighed in on this prevailing issue. Vitalik Buterin, co-founder of Ethereum, in his piece ‘’What else could memecoins be?’’ holds a broad perspective. Rather than dismissing the entire concept of memecoins, he recognizes how important they have become to a vast majority but expresses a desire for higher-quality projects that contribute to the entire ecosystem through public good. Similarly, Changpeng Zhao (CZ), former CEO of Binance recently tweeted “I am not against memes, but meme coins are getting "a little" weird now. Let's build real applications using blockchain.”
+          Also, prominent figures in the cryptocurrency space have weighed in on
+          this prevailing issue. Vitalik Buterin, co-founder of Ethereum, in his
+          piece ‘’What else could memecoins be?’’ holds a broad perspective.
+          Rather than dismissing the entire concept of memecoins, he recognizes
+          how important they have become to a vast majority but expresses a
+          desire for higher-quality projects that contribute to the entire
+          ecosystem through public good. Similarly, Changpeng Zhao (CZ), former
+          CEO of Binance recently tweeted “I am not against memes, but meme
+          coins are getting "a little" weird now. Let's build real applications
+          using blockchain.”
         </p>
         <p>
-          Another prominent figure, Tarun Chitra, CEO of Gauntlet, has voiced significant scepticism regarding DeSci initiatives. Chitra argues that the lack of regulatory frameworks and accountability in DeSci can jeopardize both scientific integrity and funding mechanisms. He highlights that while the philosophical premise behind DeSci is commendable — aiming for a better scientific system — many projects fall short and just ride the hype train for ulterior motives. His call for "no scams" in the DeSci space emphasises the necessity for ethical investing and clear communication within the community. <br/>
+          Another prominent figure, Tarun Chitra, CEO of Gauntlet, has voiced
+          significant scepticism regarding DeSci initiatives. Chitra argues that
+          the lack of regulatory frameworks and accountability in DeSci can
+          jeopardize both scientific integrity and funding mechanisms. He
+          highlights that while the philosophical premise behind DeSci is
+          commendable — aiming for a better scientific system — many projects
+          fall short and just ride the hype train for ulterior motives. His call
+          for "no scams" in the DeSci space emphasises the necessity for ethical
+          investing and clear communication within the community. <br />
         </p>
         <p>
-          In conclusion, a shared perspective among many of these debates in the crypto and scientific communities is that memecoins, particularly through DeSci, can have a collective real-world impact. By riding on the engaging nature of memes and relatability, we can foster greater support for scientific endeavours, benefiting both scientists and science enthusiasts alike. DeSci presents an alternative to traditional research and knowledge sharing by democratizing access to scientific inquiry. Encouraging the development of projects that emphasize open access and provide innovative funding mechanisms can transform the memecoin phenomenon into a powerful tool for advancing science, the scientific community, and beyond!
+          In conclusion, a shared perspective among many of these debates in the
+          crypto and scientific communities is that memecoins, particularly
+          through DeSci, can have a collective real-world impact. By riding on
+          the engaging nature of memes and relatability, we can foster greater
+          support for scientific endeavours, benefiting both scientists and
+          science enthusiasts alike. DeSci presents an alternative to
+          traditional research and knowledge sharing by democratizing access to
+          scientific inquiry. Encouraging the development of projects that
+          emphasize open access and provide innovative funding mechanisms can
+          transform the memecoin phenomenon into a powerful tool for advancing
+          science, the scientific community, and beyond!
         </p>
       </div>
 
@@ -29,42 +79,72 @@ const Desci = () => {
         <h3>References:</h3>
         <ol>
           <li>
-            <a href="https://www.youtube.com/watch?v=4BVpEoQ4T2M" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.youtube.com/watch?v=4BVpEoQ4T2M"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Richard Dawkins | Memes | Oxford Union
             </a>
           </li>
           <li>
-            <a href="https://www.youtube.com/watch?v=pbLh9R-0Pt8" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.youtube.com/watch?v=pbLh9R-0Pt8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Where Did the Word Meme Come From?
             </a>
           </li>
           <li>
-            <a href="https://www.youtube.com/watch?v=bJTSxRBbCQA" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.youtube.com/watch?v=bJTSxRBbCQA"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Casually Explained | Memes
             </a>
           </li>
           <li>
-            <a href="https://www.usnews.com/news/national-news/articles/2024-11-13/what-is-doge-elon-musk-and-vivek-ramaswamys-new-trump-agency#:~:text=The%20acronym%20%E2%80%9CDOGE%E2%80%9D%20is%20likely%20a%20nod%20to%20a%20favorite%20Musk%20meme%20and%20cryptocurrency%2C%20Dogecoin" target="_blank" rel="noopener noreferrer">
-              The acronym “DOGE” is likely a nod to a favorite Musk meme and cryptocurrency, Dogecoin
+            <a
+              href="https://www.usnews.com/news/national-news/articles/2024-11-13/what-is-doge-elon-musk-and-vivek-ramaswamys-new-trump-agency#:~:text=The%20acronym%20%E2%80%9CDOGE%E2%80%9D%20is%20likely%20a%20nod%20to%20a%20favorite%20Musk%20meme%20and%20cryptocurrency%2C%20Dogecoin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              The acronym “DOGE” is likely a nod to a favorite Musk meme and
+              cryptocurrency, Dogecoin
             </a>
           </li>
           <li>
-            <a href="https://vitalik.eth.limo/general/2024/03/29/memecoins.html" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://vitalik.eth.limo/general/2024/03/29/memecoins.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               What else could memecoins be?
             </a>
           </li>
           <li>
-            <a href="https://x.com/cz_binance/status/1861306581758976434" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://x.com/cz_binance/status/1861306581758976434"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               "I am not against memes'' - CZ
             </a>
           </li>
           <li>
-            <a href="https://x.com/tarunchitra/status/1866022281563996317" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://x.com/tarunchitra/status/1866022281563996317"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               "No scam" - Tarun Chitra
             </a>
           </li>
         </ol>
       </div>
+      <Footer />
     </>
   );
 };
