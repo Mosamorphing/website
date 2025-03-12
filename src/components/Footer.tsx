75426fbdbda5ba91fc67339b1749259051fe024e
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2025-03-12 01:40:18 +0100

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,13 @@ const Footer = () => {
           <div className="flex items-center xs:justify-center gap-[30px] w-full">
             <div className="footer__line xs:hidden"></div>
             <div className="flex gap-[30px] text-sm text-black footer__links">
+            <a
+                href="https://github.com/Mosamorphing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
               <a
                 href="https://www.instagram.com/mofasasi"
                 target="_blank"
@@ -27,7 +34,7 @@ const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                X/Twitter
+                X
               </a>
             </div>
           </div>
